984f9583af297357e21891c8e5ba26e5f45e87d8
ximet <dq.ximet@gmail.com>
2017-05-26 18:03:23 +0300

    setup lastfm func

--- scrobbler.py
+++ scrobbler.py
@@ -9,4 +9,9 @@ class Scrobbler(object):
         self.arg = arg
 
     def setup_lastfm(arg):
-        pass
+        API_KEY = ""
+        API_SECRET = ""
+        username = ""
+        password = pylast.md5("")
+        self.lastfm = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET,
+                               username=username, password_hash=password_hash)
