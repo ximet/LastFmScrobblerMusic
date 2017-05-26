6f86cc9f2a1b41fc3a95aa9de4961bb03ba4f1f0
ximet <dq.ximet@gmail.com>
2017-05-26 17:48:18 +0300

    was born scrobbler

---
+++ scrobbler.py
@@ -0,0 +1,12 @@
+import pylast
+
+class Scrobbler(object):
+    a_music = None
+    lastfm = None
+
+    def __init__(self, arg):
+        super(, self).__init__()
+        self.arg = arg
+
+    def setup_lastfm(arg):
+        pass
