6aa3ade20b3aa6f864d9c3be4d40acb993a77ac5
ximet <dq.ximet@gmail.com>
2017-05-27 13:14:03 +0300

    parse json file

--- scrobbler.py
+++ scrobbler.py
@@ -1,17 +1,23 @@
 import pylast
+import json
 
 class Scrobbler(object):
     a_music = None
     lastfm = None
+    config = None
 
     def __init__(self, arg):
         super(, self).__init__()
         self.arg = arg
 
+    def parse_json(filename):
+        with open(filename) as data_file:
+            config = json.load(data_file)
+
     def setup_lastfm(arg):
-        API_KEY = ""
-        API_SECRET = ""
-        username = ""
-        password = pylast.md5("")
+        API_KEY = self.config["API_KEY"]
+        API_SECRET = self.config["API_SECRET"]
+        username = self.config["username"]
+        password = pylast.md5(self.config["password"])
         self.lastfm = pylast.LastFMNetwork(api_key=API_KEY, api_secret=API_SECRET,
-                               username=username, password_hash=password_hash)
+                               username=username, password_hash=password)
